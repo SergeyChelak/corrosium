6b16a9d0565827c096d85aa63922bacb28ea2907
Sergey Chelak <sergey.chelak@gmail.com>
2023-09-01 11:54:47 +0300

    test disk service

--- bootloader/src/stage1/bios.asm
+++ bootloader/src/stage1/bios.asm
@@ -1,12 +1,14 @@
+;*********************************************************************************
 ; Corrosium OS
 ; BIOS utils
+;*********************************************************************************
 
 bits 16
 
 BIOS_print:
 ;*********************************************************************************
-; Prints a zero-terminated string (in real mode)
-;---------------------------------------------------------------------------------;
+; Prints a zero-terminated string via BIOS function
+;---------------------------------------------------------------------------------
 ; si: pointer to string
 ;*********************************************************************************
     push ax
@@ -26,3 +28,25 @@ BIOS_print:
     pop bx
     pop ax
     ret
+
+;*********************************************************************************
+; Stores current drive identifier and checks if disk service is supported
+; If service isn't supported, prints corresponding message and halts the CPU
+;*********************************************************************************
+BIOS_test_disk_service:
+    mov [stage1_disk_id], dl
+    mov ah, 0x41
+    mov bx, 0x55aa
+    int 0x13
+    jc .BIOS_disk_service_not_supported
+    cmp bx, 0xaa55
+    jne .BIOS_disk_service_not_supported
+    ret
+    .BIOS_disk_service_not_supported:
+        mov si, stage1_disk_ext_error_message
+        .halt:
+            hlt
+            jmp .halt
+
+stage1_disk_id db 0x0
+stage1_disk_ext_error_message  db 'Disk extension is not supported', 13, 10, 0
