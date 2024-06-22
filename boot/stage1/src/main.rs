de8f19fd75ed68332d314063b87f79071d880de7
Sergey Chelak <sergey.chelak@gmail.com>
2024-06-23 01:20:09 +0300

    bootloader structure

--- boot/stage1/src/main.rs
+++ boot/stage1/src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Bootloader stage #1");
+#![no_std]
+#![no_main]
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    // Bootloader stage #1
+    loop {}
+}
+
+#[panic_handler]
+fn panic(_info: &core::panic::PanicInfo) -> ! {
+    loop {}
 }
--- boot/stage2/src/main.rs
+++ boot/stage2/src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Bootloader stage #2");
+#![no_std]
+#![no_main]
+
+#[no_mangle]
+#[link_section = ".start"]
+pub extern "C" fn _stage2() -> ! {
+    // Bootloader stage #2
+    loop {}
+}
+
+#[panic_handler]
+fn panic(_info: &core::panic::PanicInfo) -> ! {
+    loop {}
 }
