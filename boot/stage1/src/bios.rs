8db378fd9b7da01c327181ec8eb12293b8e21875
Sergey Chelak <sergey.chelak@gmail.com>
2024-06-23 23:35:13 +0300

    loading 2nd stage

--- boot/stage1/src/bios.rs
+++ boot/stage1/src/bios.rs
@@ -17,3 +17,21 @@ pub fn print(message: &str) {
         )
     }
 }
+
+pub fn read_sectors(disk: u8, from: u8, sectors: u8, target: u16) -> u8 {
+    unsafe {
+        let mut error_code: u8;
+        asm!(
+            "mov ah, 0x2",              // read sector command
+            "mov ch, 0x0",              // cylinder
+            "mov dh, 0x0",              // head number
+            "int 0x13",
+            out("ah") error_code,
+            in("bx") target,
+            in("al") sectors,           // number of sectors to read
+            in("cl") from,              // start from nth sector
+            in("dl") disk
+        );
+        error_code
+    }
+}
--- boot/stage1/src/main.rs
+++ boot/stage1/src/main.rs
@@ -4,7 +4,11 @@
 mod bios;
 mod x86;
 
-use core::arch::global_asm;
+use core::arch::{asm, global_asm};
+
+use bios::print;
+
+const MAX_NEXT_STAGE_SECTORS: u8 = 63;
 
 global_asm!(include_str!("main.asm"));
 
@@ -14,9 +18,20 @@ extern "C" {
 
 #[no_mangle]
 pub extern "C" fn _stage1() -> ! {
-    bios::print("Bootloader stage #1\r\n\0");
-
+    let disk_id = unsafe {
+        let number: u8;
+        asm!("mov {0}, dl", out(reg_byte) number);
+        number
+    };
     let next_stage: *const u16 = unsafe { &_next_stage };
+
+    let target = next_stage as u16;
+    let error_code = bios::read_sectors(disk_id, 2, MAX_NEXT_STAGE_SECTORS, target);
+    if error_code != 0x0 {
+        print("! Disk read error\n\r\0");
+        halt();
+    }
+    print("2nd stage loaded\r\n\0");
     x86::jump(next_stage);
     halt()
 }
@@ -27,7 +42,7 @@ fn panic(_info: &core::panic::PanicInfo) -> ! {
 }
 
 fn halt() -> ! {
-    bios::print("[WARN] halted\r\n\0");
+    print("* Halted\r\n\0");
     x86::cli();
     loop {
         x86::hlt()
