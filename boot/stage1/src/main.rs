318f4c660744ba62382e4665b1d72a5ee05b41c3
Sergey Chelak <sergey.chelak@gmail.com>
2024-06-29 20:53:06 +0300

    minor refactor

--- boot/stage1/src/main.rs
+++ boot/stage1/src/main.rs
@@ -3,7 +3,10 @@
 
 mod bios;
 
-use core::arch::{asm, global_asm};
+use core::{
+    arch::{asm, global_asm},
+    ptr::addr_of,
+};
 
 use bios::print;
 
@@ -11,8 +14,12 @@ const MAX_NEXT_STAGE_SECTORS: u8 = 63;
 
 global_asm!(include_str!("main.asm"));
 
-extern "C" {
-    static _next_stage: u16;
+fn next_stage() -> u16 {
+    extern "C" {
+        #[link_name = "_next_stage"]
+        static next_stage: u16;
+    }
+    unsafe { addr_of!(next_stage) as u16 }
 }
 
 #[no_mangle]
@@ -22,15 +29,14 @@ pub extern "C" fn _stage1() -> ! {
         asm!("mov {0}, dl", out(reg_byte) number);
         number
     };
-    let stage2: *const u16 = unsafe { &_next_stage };
-    let target = stage2 as u16;
-    let error_code = bios::read_sectors(disk_id, 2, MAX_NEXT_STAGE_SECTORS, target);
+    let stage2 = next_stage();
+    let error_code = bios::read_sectors(disk_id, 2, MAX_NEXT_STAGE_SECTORS, stage2);
     if error_code != 0x0 {
         print("! Disk read error\n\r\0");
         halt();
     }
     print("2nd stage loaded\r\n\0");
-    jump(target);
+    jump(stage2);
     halt()
 }
 
