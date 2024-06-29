1a915eb6d115db906c561735d8f8b05f0412c422
Sergey Chelak <sergey.chelak@gmail.com>
2024-06-30 01:18:09 +0300

    added linker script for kernel

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -2,7 +2,14 @@
 #![no_main]
 
 #[no_mangle]
+#[link_section = ".kernel_start"]
 pub extern "C" fn kernel_main() -> ! {
+    // green background, white foreground
+    let clr = (2 as u8) << 4 | (15 as u8);
+    let value = (b'K' as u16) | (clr as u16) << 8;
+    unsafe {
+        core::ptr::write_volatile(0xb8000 as *mut u16, value);
+    }
     loop {}
 }
 
