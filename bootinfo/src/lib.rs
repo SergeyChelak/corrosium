857ad679371977ab31efa59a3115b91e62eb13f4
Sergey Chelak <sergey.chelak@gmail.com>
2026-05-31 20:40:36 +0300

    populate acpi & smbios

--- bootinfo/src/lib.rs
+++ bootinfo/src/lib.rs
@@ -1,9 +1,42 @@
 #![no_std]
+use core::ffi::c_void;
 
 #[repr(C)]
+#[derive(Default)]
 pub struct BootInfo {
-    frame_buffer: FrameBuffer,
+    pub acpi: Option<ConfigTable>,
+    pub smbios: Option<ConfigTable>,
+    // pub frame_buffer: FrameBuffer,
 }
 
 #[repr(C)]
-pub struct FrameBuffer {}
+pub struct ConfigTable {
+    pub address: *const c_void,
+    pub version: u8,
+}
+
+impl ConfigTable {
+    fn is_newer_version(&self, other: &Option<ConfigTable>) -> bool {
+        let Some(other) = other else {
+            return true;
+        };
+        self.version > other.version
+    }
+}
+
+impl BootInfo {
+    pub fn set_acpi(&mut self, acpi: ConfigTable) {
+        if acpi.is_newer_version(&self.acpi) {
+            self.acpi = Some(acpi);
+        }
+    }
+
+    pub fn set_smbios(&mut self, smbios: ConfigTable) {
+        if smbios.is_newer_version(&self.smbios) {
+            self.smbios = Some(smbios);
+        }
+    }
+}
+
+// #[repr(C)]
+// pub struct FrameBuffer {}
--- bootloader/src/main.rs
+++ bootloader/src/main.rs
@@ -1,11 +1,13 @@
 #![no_std]
 #![no_main]
-
-use core::time::Duration;
+use core::hint::black_box;
 
 use bootinfo::*;
 use log::info;
-use uefi::prelude::*;
+use uefi::{prelude::*, table::cfg::ConfigTableEntry};
+
+mod config_table_holder;
+use config_table_holder::*;
 
 #[entry]
 fn main() -> Status {
@@ -15,7 +17,66 @@ fn main() -> Status {
 
     info!("Corrosium Bootloader started");
 
-    boot::stall(Duration::from_secs(10));
+    let mut boot_info = BootInfo::default();
+
+    // Iterate across the Config Tables and enumerate them, displaying them to the log
+    uefi::system::with_config_table(|entry| {
+        for cfg in entry {
+            if let Some(acpi) = acpi_config_table(cfg) {
+                boot_info.set_acpi(acpi);
+                info!("Did set ACPI config table");
+                continue;
+            }
+
+            if let Some(smbios) = smbios_config_table(cfg) {
+                boot_info.set_smbios(smbios);
+                info!("Did set SMBIOS config table");
+                continue;
+            }
+
+            let holder = ConfigTableEntryHolder(cfg);
+            info!("Skipped table for {}", holder);
+        }
+    });
+
+    info!("Press any key...");
+    wait_for_key();
 
     Status::SUCCESS
+}
+
+fn acpi_config_table(entry: &ConfigTableEntry) -> Option<ConfigTable> {
+    let Some((address, version)) = (match entry.guid {
+        ConfigTableEntry::ACPI2_GUID => Some((entry.address, 2)),
+        ConfigTableEntry::ACPI_GUID => Some((entry.address, 1)),
+        _ => None,
+    }) else {
+        return None;
+    };
+    let acpi = ConfigTable { address, version };
+    Some(acpi)
+}
+
+fn smbios_config_table(entry: &ConfigTableEntry) -> Option<ConfigTable> {
+    let Some((address, version)) = (match entry.guid {
+        ConfigTableEntry::SMBIOS3_GUID => Some((entry.address, 3)),
+        ConfigTableEntry::SMBIOS_GUID => Some((entry.address, 1)),
+        _ => None,
+    }) else {
+        return None;
+    };
+    let smbios = ConfigTable { address, version };
+    Some(smbios)
+}
+
+fn wait_for_key() {
+    uefi::system::with_stdin(|stdin| {
+        let Ok(key_event) = stdin.wait_for_key_event() else {
+            return;
+        };
+        _ = uefi::boot::wait_for_event(&mut [key_event]);
+        while let Ok(Some(key)) = stdin.read_key() {
+            black_box(key);
+        }
+    });
 }
