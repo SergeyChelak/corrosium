c81fd76ad68381dbdeea0b19ec589500ac1c67ef
Sergey Chelak <sergey.chelak@gmail.com>
2026-05-31 18:46:17 +0300

    minial uefi app

--- bootloader/src/main.rs
+++ bootloader/src/main.rs
@@ -1,6 +1,21 @@
-#[no_std]
-use bootinfo::*;
+#![no_std]
+#![no_main]
 
-fn main() {
-    println!("Hello, world!");
+use core::time::Duration;
+
+use bootinfo::*;
+use log::info;
+use uefi::prelude::*;
+
+#[entry]
+fn main() -> Status {
+    let Ok(_) = uefi::helpers::init() else {
+        return Status::NOT_READY;
+    };
+
+    info!("Corrosium Bootloader started");
+
+    boot::stall(Duration::from_secs(10));
+
+    Status::SUCCESS
 }
